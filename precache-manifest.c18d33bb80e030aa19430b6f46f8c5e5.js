9227f27733ff26691029de0d3d8e1c8980e50c7a
Rishabh Gupta <gupta.rishabh2912@gmail.com>
2020-07-21 20:27:15 +0530

    Updates

---
+++ precache-manifest.c18d33bb80e030aa19430b6f46f8c5e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94ec15f39272d2db6114b4b959a7e3f4",
+    "url": "/Meme-Generator/index.html"
+  },
+  {
+    "revision": "3ba00a240b23b6e88a29",
+    "url": "/Meme-Generator/static/js/2.fa550cd3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Meme-Generator/static/js/2.fa550cd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "697c9696d531676698aa",
+    "url": "/Meme-Generator/static/js/main.40ad3b90.chunk.js"
+  },
+  {
+    "revision": "158a64cff1ce755022e0",
+    "url": "/Meme-Generator/static/js/runtime-main.89444324.js"
+  }
+]);
